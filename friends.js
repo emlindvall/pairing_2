80778a3612336d4ad37b70086d8b11d63beacd46
Em Lindvall <elindvall22@turing.edu>
2023-01-11 17:07:53 -0700

    Add notes about destinations

--- friends.js
+++ friends.js
@@ -3,3 +3,8 @@ console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
 
+var toVisit = ["Barcelona", "Scotland", "China", "South Korea", "Mexico"];
+toVisit.push("Lisbon");
+console.log(toVisit);
+toVisit.pop();
+console.log(toVisit);
